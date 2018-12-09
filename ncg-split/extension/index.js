e2126bdbe70d9b572b935d2c517468359e645880
Paeweł PRrędki <papredki@gmail.com>
2018-12-09 20:53:14 +0100

    added obs-websocket + muting with physical button

--- ncg-split/extension/index.js
+++ ncg-split/extension/index.js
@@ -2,6 +2,44 @@
 
 const fs = require('fs');
 const Gpio = require('onoff').Gpio;
+process.env["NODE_CONFIG_DIR"] = "./bundles/ncg-split/config/";
+const config = require('config');
+const OBSWebSocket = require('obs-websocket-js');
+
+// Default connection settings.
+var settings = {
+	address: config.get('obsConnection.address')
+}
+
+// If there is a password in the config, use it.
+if (config.has('obsConnection.password') && config.get('obsConnection.password') !== '')
+	settings.password = config.get('obsConnection.password');
+
+// Do the startup stuff.
+console.log('Started up.');
+const obs = new OBSWebSocket();
+connect();
+function connect() {
+	obs.connect(settings).then(() => {
+		console.log('OBS connection successful.');
+	}).catch((err) => {});
+}
+
+// We need to try and reconnect if the connection is closed.
+// This also fires if we can't successfully connect in the first place.
+obs.on('ConnectionClosed', data => {
+	console.log('OBS connection lost, retrying in 5 seconds.');
+	setTimeout(connect, 5000);
+});
+
+// Error catching.
+obs.on('error', err => {
+	console.log('OBS connection error:', err);
+	// I don't know if we need to reconnect here?
+	// I don't think so, an error doesn't always mean a disconnect.
+});
+
+module.exports = obs;
 
 module.exports = nodecg => {
 
@@ -23,6 +61,30 @@ module.exports = nodecg => {
 				return;
 			}
 			nodecg.sendMessage('ncgsplit-ext-reset');
+		});
+
+
+		const ledOne = new Gpio(17, 'out');
+		ledOne.writeSync(0);
+		const ledTwo = new Gpio(27, 'out');
+		ledTwo.writeSync(0);
+		const ledThree = new Gpio(22, 'out');
+		ledThree.writeSync(0);
+
+		const muteButton = new Gpio(4, 'in', 'rising', {debounceTimeout:10});
+		muteButton.watch((err, value) => {
+			console.log("Toggle mute");
+			const micSource = config.get('obsConnection.micSource');
+			obs.send('GetMute', {'source':micSource}, (err, data) => {
+				if (!err) {
+					const muted = data.muted;
+					obs.send('ToggleMute', {'source':micSource}, (err,data) => {
+						ledThree.writeSync(muted ? 1 : 0);
+					});
+				} else {
+					console.log("OBS Error: " + err);
+				}
+			});
 		});
 	} else {
 		console.log("Gpio not available on this system");
